76c956a38d208f03f3b703e357c1a9e2df53ad93
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-23 09:07:38 +0800

    End of section 12

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,14 +8,15 @@ import { ProductDetailsComponent } from './shop/product-details/product-details.
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent  },
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   // {path: 'shop', component: ShopComponent},
   // {path: 'shop/:id', component: ProductDetailsComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)
+    , data: {breadcrumb: 'Shop'}},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
