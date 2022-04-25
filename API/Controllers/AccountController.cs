7217be555bfa2ca9662436c55aa0b1b1ce3dd0b7
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-25 15:00:47 +0800

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -108,6 +108,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{
+                    Errors = new []{"Email Address already exists."}});
+            }
+            
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
