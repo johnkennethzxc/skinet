e842e6c65e138a66a46563a049afd7eb6aebe0d2
johnkennethzxc <bombanejohnkenneth01@gmail.com>
2022-04-27 22:33:07 +0800

    End of section 19

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, map, ReplaySubject, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { IAddress } from '../shared/models/address';
 import { IUser } from '../shared/models/user';
 
 @Injectable({
@@ -71,5 +72,11 @@ export class AccountService {
     return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
   }
 
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address')
+  }
 
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
+  }
 }
